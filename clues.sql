ec237f47c128fb2e0f49673f2a15ed96f2525de7
JunguLiu <33013871+JunguLiu@users.noreply.github.com>
2020-09-13 21:51:22 -0600

    u

--- clues.sql
+++ clues.sql
@@ -1,8 +1,15 @@
 -- Clue #1: We recently got word that someone fitting Carmen Sandiego's description has been
 -- traveling through Southern Europe. She's most likely traveling someplace where she won't be noticed,
 -- so find the least populated country in Southern Europe, and we'll start looking for her there.
- 
+
 -- Write SQL query here
+-- SELECT name
+-- FROM country
+-- WHERE region = 'Southern Europe'
+-- ORDER BY population
+-- LIMIT 1
+
+-- Holy See (Vatican City State) 
 
 
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in
@@ -10,14 +17,28 @@
 -- spoken in this country, so we can call in a translator to work with you.
 
 -- Write SQL query here
+-- SELECT language
+-- FROM countrylanguage
+-- WHERE countrycode = 'VAT'
 
+-- Italian
 
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on
 -- to a different country, a country where people speak only the language she was learning. Find out which
 -- nearby country speaks nothing but that language.
 
--- Write SQL query here
 
+--     SELECT name
+--     FROM country, countrylanguage
+--     WHERE countrycode = code AND region = 'Southern Europe' AND language = 'Italian' AND countrycode != 'VAT'
+-- INTERSECT
+--     SELECT name
+--     FROM countrylanguage, country
+--     WHERE countrycode = code
+--     GROUP BY name
+--     HAVING COUNT(language) = 1
+
+-- San Marino
 
 -- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time.
 -- There are only two cities she could be flying to in the country. One is named the same as the country – that
@@ -25,6 +46,11 @@
 -- be flying to.
 
 -- Write SQL query here
+-- SELECT name
+-- FROM city
+-- WHERE countrycode = 'SMR' AND name != 'San Marino'
+
+-- Serravalle 
 
 
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different
@@ -33,13 +59,24 @@
 
 -- Write SQL query here
 
+-- SELECT city.name
+-- FROM city, country
+-- WHERE countrycode = code AND region = 'South America' AND city.name like 'Serr%'
+
+-- Serra
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
 -- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll
 -- follow right behind you!
 
 -- Write SQL query here
+-- SELECT name
+-- FROM city
+-- WHERE id = (SELECT capital
+-- FROM city, country
+-- WHERE countrycode = code AND city.name = 'Serra')
 
+-- Brasï¿½lia
 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to
 -- the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the
@@ -57,3 +94,8 @@
 
 -- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
 
+-- SELECT name
+-- FROM city
+-- WHERE population = 91084
+
+-- Santa Monica
